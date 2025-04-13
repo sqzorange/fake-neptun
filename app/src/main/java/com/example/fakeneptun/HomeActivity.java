0828096d56372f991edb5e2e476499c7d8f73fb0
h356009 <h356009@stud-u.szeged.hu>
2025-04-13 18:47:57 +0200

    RegisterActivity - működik a regisztrálás

--- app/src/main/java/com/example/fakeneptun/HomeActivity.java
+++ app/src/main/java/com/example/fakeneptun/HomeActivity.java
@@ -1,13 +1,29 @@
 package com.example.fakeneptun;
 
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class HomeActivity extends AppCompatActivity {
+    private static final String LOG_TAG = HomeActivity.class.getName();
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null){
+            Log.d(LOG_TAG, "Felhasználó bejelentkezve!");
+        }
+        else {
+            Log.d(LOG_TAG, "Felhasználó nincs bejelentkezve!");
+            finish();
+        }
     }
 }
--- app/src/main/java/com/example/fakeneptun/RegisterActivity.java
+++ app/src/main/java/com/example/fakeneptun/RegisterActivity.java
@@ -4,17 +4,29 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class RegisterActivity extends AppCompatActivity {
+    private static final String LOG_TAG = RegisterActivity.class.getName();
+    private FirebaseAuth mAuth;
 
     EditText userNameET;
     EditText userEmailET;
     EditText passwordET;
     EditText passowrdConfirmET;
-    private static final String LOG_TAG = RegisterActivity.class.getName();
+    EditText familyNameET;
+    EditText firstNameET;
+    CheckBox isTeacher;
 
 
     @Override
@@ -26,15 +38,17 @@ public class RegisterActivity extends AppCompatActivity {
         userEmailET = findViewById(R.id.editEmail);
         passwordET = findViewById(R.id.editTextPassword);
         passowrdConfirmET = findViewById(R.id.passwordAgain);
-
-        //Bundle bundle = getIntent().getExtras();
-        //bundle.getInt("SECRET_KEY");
+        familyNameET = findViewById(R.id.familyName);
+        firstNameET = findViewById(R.id.firstName);
+        isTeacher = findViewById(R.id.isTeacher);
 
         int secret_key = getIntent().getIntExtra("SECRET_KEY", 0);
 
         if (secret_key != 69){
             finish();
         }
+
+        mAuth = FirebaseAuth.getInstance();
 
     }
 
@@ -43,17 +57,37 @@ public class RegisterActivity extends AppCompatActivity {
         String userEmail = userEmailET.getText().toString();
         String password = passwordET.getText().toString();
         String passwordConfirm = passowrdConfirmET.getText().toString();
+        String familyName = familyNameET.getText().toString();
+        String firstName = firstNameET.getText().toString();
+        boolean teacher = isTeacher.isChecked();
 
         if (!password.equals(passwordConfirm)) {
             Log.e(LOG_TAG, "Nem egyezik meg a két jelszó!");
             return;
         }
-
-        Log.i(LOG_TAG, "Regisztrált: " + userName + ", Email: " + userEmail + ", Jelszó: " + password);
+        Log.i(LOG_TAG, "Regisztrált: " + userName + ", Email: " + userEmail + ", Jelszó: " + password + ", családi: " + familyName + ", keresztnév: " + firstName + ", tanár: " + teacher);
+        mAuth.createUserWithEmailAndPassword(userEmail, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            Log.d(LOG_TAG, "Felhasználó létrehozva!");
+                            moveToHome();
+                        } else {
+                            Log.d(LOG_TAG, "Felhasználó létrehozása sikertelen: " + task.getException().getMessage());
+                            Toast.makeText(RegisterActivity.this, "Felhasználó létrehozása sikertelen: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
-    public void moveToMain(View view) {
+    public void moveToMain() {
         Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void moveToHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
     }
 }
