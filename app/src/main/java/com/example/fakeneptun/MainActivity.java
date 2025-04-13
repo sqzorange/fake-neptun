4149aad041a021cc5ccfe61e76f917a439c88c2b
h356009 <h356009@stud-u.szeged.hu>
2025-04-13 15:18:31 +0200

    Main Activity - login log

--- app/src/main/java/com/example/fakeneptun/MainActivity.java
+++ app/src/main/java/com/example/fakeneptun/MainActivity.java
@@ -1,28 +1,26 @@
 package com.example.fakeneptun;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-
-import androidx.activity.EdgeToEdge;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 
     public void login(View view) {
+        EditText userName = findViewById(R.id.editTextUserName);
+        EditText password = findViewById(R.id.editTextPassword);
+
+        String userNameStr = userName.getText().toString();
+        String passwordStr = password.getText().toString();
+
+        Log.i("MainActivity", "Bejelentkezett: " + userNameStr + ", Jelszó: " + passwordStr);
     }
 }
