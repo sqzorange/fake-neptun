0284e2641d1ac56b6bdf8a8488c9dc70bf6e22b2
h356009 <h356009@stud-u.szeged.hu>
2025-04-13 18:59:35 +0200

    Működik a bejelentkezés

--- app/src/main/java/com/example/fakeneptun/MainActivity.java
+++ app/src/main/java/com/example/fakeneptun/MainActivity.java
@@ -5,11 +5,20 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private static final String LOG_TAG = MainActivity.class.getName();
     private static final int SECRET_KEY = 69;
+    private FirebaseAuth mAuth;
     EditText usernameET;
     EditText passwordET;
 
@@ -17,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
 
         usernameET = findViewById(R.id.editTextUserName);
         passwordET = findViewById(R.id.editTextPassword);
@@ -26,12 +37,31 @@ public class MainActivity extends AppCompatActivity {
         String userName = usernameET.getText().toString();
         String password = passwordET.getText().toString();
 
-        Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", Jelszó: " + password);
+        //Log.i(LOG_TAG, "Bejelentkezett: " + userName + ", Jelszó: " + password);
+
+        mAuth.signInWithEmailAndPassword(userName, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Log.d(LOG_TAG, "Bejelentkezve!");
+                    moveToHome();
+                } else {
+                    Log.d(LOG_TAG, "A bejelentkezés nem sikerült: " + task.getException().getMessage());
+                    Toast.makeText(MainActivity.this, "A bejelentkezés nem sikerült: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     public void onRegister(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
         intent.putExtra("SECRET_KEY", 69);
         startActivity(intent);
+    }
+
+    public void moveToHome() {
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
