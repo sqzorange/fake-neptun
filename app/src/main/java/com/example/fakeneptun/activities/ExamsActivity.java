1b72cc8e477b5889cee7bc05d73ce5b15b76a277
sqzorange <fintus32@gmail.com>
2025-05-18 13:33:41 +0200

    miniCommit: Vizsgák

--- app/src/main/java/com/example/fakeneptun/activities/ExamsActivity.java
+++ app/src/main/java/com/example/fakeneptun/activities/ExamsActivity.java
@@ -1,7 +1,11 @@
 package com.example.fakeneptun.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.fakeneptun.R;
 
 public class ExamsActivity extends AppCompatActivity {
@@ -10,5 +14,11 @@ public class ExamsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams);
+
+        TextView easterEggText = findViewById(R.id.easter_egg_text);
+        easterEggText.setText("Tesó nem akarsz ebben a félévben vizsgázni?");
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> finish());
     }
 }
--- app/src/main/java/com/example/fakeneptun/activities/ExamsManagementActivity.java
+++ app/src/main/java/com/example/fakeneptun/activities/ExamsManagementActivity.java
@@ -1,6 +1,8 @@
 package com.example.fakeneptun.activities;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.fakeneptun.R;
 
@@ -10,5 +12,11 @@ public class ExamsManagementActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exams_management);
+
+        TextView easterEggText = findViewById(R.id.easter_egg_text);
+        easterEggText.setText("Biztos szivatni akarod a hallgatókat ezzel?");
+
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(v -> finish());
     }
 }
